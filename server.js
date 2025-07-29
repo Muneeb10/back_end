28194f4c8d37394ab898ceea836e0ce76e1c1106
Muneeb Yousaf <98748398+Muneeb10@users.noreply.github.com>
2025-07-29 03:55:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ app.use(express.json());
 
 
 
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
