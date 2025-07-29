4c094b0a86fe18335f376169e5970a5723561410
Muneeb Yousaf <muneebyousaf91@gmail.com>
2025-07-29 02:16:12 -0700

    update server.js file

--- server.js
+++ server.js
@@ -7,12 +7,20 @@ const FormSubmission = require('./models/FormSubmission');
 const Subscriber = require('./models/Subscriber');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json()); 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+
+  res.send("API is running");
+
+});
+
 
 app.post('/api/form', async (req, res) => {
   try {
@@ -51,6 +59,8 @@ app.post('/api/form', async (req, res) => {
 
 
 // POST route for newsletter subscription
+
+
 app.post('/subscribe', async (req, res) => {
   const { email } = req.body;
 
